34e2bce9d3c2e4ef6cb6e683af7d60420ae0a5af
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-04-02 21:46:25 -0400

    demo week 2

---
+++ week2/demo/class/shape.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of shape
+ *
+ * @author GFORTI
+ */
+class shape {
+    //put your code here
+    
+    
+    private $shape;
+    
+    public function __construct($shapeType) {
+        $this->setShape($shapeType);
+    }
+    
+    public function getShape() {
+        return $this->shape;
+    }
+
+    protected function setShape($shape) {
+        $this->shape = $shape;
+    }
+
+    
+}
