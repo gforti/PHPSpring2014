438bc49782ade917c8006e3758c94cabb4c31fd7
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-04-18 18:06:48 -0400

    demo week 3 -2 stuff

---
+++ week3-2/demo2/webform.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        
+        
+        <form name="mainform" action="#" method="post"> 
+           <fieldset>
+		<legend>Sign-up Form:</legend>
+                <label for="email">E-mail:</label> 
+                <input id="email" type="text" name="email" value="" /> <br />
+                
+                
+                <label for="username">Username:</label>
+                <input id="username" type="text" name="username" value="" /> <br /> 
+                
+                
+                <label for="password">Password:</label>
+                <input id="password" type="password" name="password" /> <br />           
+            
+                
+                <label for="confirmpassword">Confirm Password:</label>
+                <input id="confirmpassword" type="password" name="confirmpassword" /> <br />           
+                
+
+                <input type="submit" value="Submit" />
+            </fieldset>
+        </form>
+    </body>
+</html>
