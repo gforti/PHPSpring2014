6be4ff3e96cc5d99c4316abed46182c6a38b1f09
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-04-23 18:11:21 -0400

    demo week 4 stuff

--- week4/demo2/configDemo.php
+++ week4/demo2/configDemo.php
@@ -3,12 +3,15 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title><?php echo Config::PAGE_TITLE; ?></title>
     </head>
     <body>
         <?php
         // put your code here
         echo Config::DB_DNS;
+        echo '<br />';
+        echo Config::DB_USER;
+        
         
         ?>
     </body>
--- week4/demo2/lib/Config.php
+++ week4/demo2/lib/Config.php
@@ -10,5 +10,6 @@ class Config {
     
     const   DB_DNS = "mysql:host=localhost;port=3306;dbname=PHPSpring2014",
             DB_USER  = "root",
-            DB_PASSWORD  = "";
+            DB_PASSWORD  = "",
+            PAGE_TITLE = "MySQL Demo";
 }
