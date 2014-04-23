cb51f1f6a2dcfee5f30160473810bbcfb37854aa
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-04-23 19:11:43 -0400

    demo week 4 stuff

--- week4/demo2/dbquery.php
+++ week4/demo2/dbquery.php
@@ -9,27 +9,41 @@
         <?php
         // put your code here
         
-        print_r($_GET);
+        //print_r($_GET);
+        
+        // dbquery.php?id=2  
+        // dbquery.php?username=test
         
         $id = filter_input(INPUT_GET, 'id');
+        $username = filter_input(INPUT_GET, 'username');
         
-        echo $id;
-        
-        
-        /*
-        
-        
-         $db = new PDO(Config::DB_DNS, Config::DB_USER, Config::DB_PASSWORD);
-        
-            $dbs = $db->prepare('select * from signup');
-            $dbs->execute();
-            $results = $dbs->fetchAll(PDO::FETCH_ASSOC);
-            
-             $statement = $db->prepare('select * from login where username = :user and password = :password limit 1');
-                $statement->bindParam(':user', $email, PDO::PARAM_STR);
-                $statement->bindParam(':password', $password, PDO::PARAM_STR);
-                $statement->execute(); */
-       
+        $db = new PDO(Config::DB_DNS, Config::DB_USER, Config::DB_PASSWORD);
+
+        $dbs = $db->prepare('select * from signup where id = :id or username = :username limit 1');
+        //$dbs = $db->prepare('insert into signup set email = :email, username = :username, password = :password');
+        $dbs->bindParam(':id', $id, PDO::PARAM_INT);
+        $dbs->bindParam(':username', $username, PDO::PARAM_STR);
+
+        $dbs->execute();
+        $results = $dbs->fetch(PDO::FETCH_ASSOC);
+
+        print_r($results);
+        if ( count($results) ) {
+        echo '<table border="1">'; 
+        echo '<tr><th>ID</th><th>Email</th>';
+        echo '<th>username</th><th>password</th></tr>';
+
+            echo '<tr>';            
+             echo '<td>', $results['id'] ,'</td>';
+             echo '<td>', $results['email'] ,'</td>';
+             echo '<td>', $results['username'] ,'</td>';
+             echo '<td>', $results['password'] ,'</td>';          
+            echo '</tr>';
+
+        echo '</table>';
+        } else {
+            echo '<p>No results found</p>';
+        }
         ?>
     </body>
 </html>
---
+++ week4/demo2/dbtest2.php
@@ -0,0 +1,37 @@
+<?php include 'dependency.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo Config::PAGE_TITLE; ?></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+        $dbc = new DB();
+        $db = $dbc->getDB();
+        
+        if ( null !== $db) {
+            $dbs = $db->prepare('select * from signup');
+            $dbs->execute();
+            $results = $dbs->fetchAll(PDO::FETCH_ASSOC);
+
+            echo '<table border="1">'; 
+            echo '<tr><th>Index</th><th>ID</th><th>Email</th>';
+            echo '<th>username</th><th>password</th></tr>';
+            foreach ($results as $key => $value) {
+                echo '<tr>';
+                 echo '<td>', $key ,'</td>';
+                 echo '<td>', $value['id'] ,'</td>';
+                 echo '<td>', $value['email'] ,'</td>';
+                 echo '<td>', $value['username'] ,'</td>';
+                 echo '<td>', $value['password'] ,'</td>';          
+                echo '</tr>';
+            }
+            echo '</table>';
+       
+        }
+        ?>
+    </body>
+</html>
