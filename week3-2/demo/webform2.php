493e0c422707de048706c4711a8da39cf535c7c6
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-04-23 20:19:09 -0400

    demo week 3 stuff

---
+++ week3-2/demo/webform2.php
@@ -0,0 +1,58 @@
+<?php include 'dependency.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+       <link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $signup = new Signup2();
+             
+       
+        
+        $errors = array();
+        if ( Util::isPostRequest() ){
+            
+            /*
+            if ( $signup->entryIsValid() ) {
+                echo '<p class="success">Data would be process and a sucess message is displayed</p>';
+            } else {
+                $errors = $signup->getErrors();
+            }
+             
+             */
+        }
+        
+            if ( count($errors) ) {
+                echo '<ul class="error">';
+                foreach ($errors as $value) {
+                    echo '<li>',$value,'</li>';
+                }
+                echo '</ul>';
+            }
+        ?>
+        
+       
+       <form name="mainform" action="#" method="post"> 
+           <fieldset>
+		<legend>Sign-up Form:</legend>
+                <label for="email">E-mail:</label> 
+                <input id="email" type="text" name="email" value="<?php echo $signup->email; ?>" /> <br />
+               
+                <label for="username">Username:</label>
+                <input id="username" type="text" name="username" value="" /> <br /> 
+                
+                <label for="password">Password:</label>
+                <input id="password" type="password" name="password" /> <br />           
+                
+                <label for="confirmpassword">Confirm Password:</label>
+                <input id="confirmpassword" type="password" name="confirmpassword" /> <br />           
+                
+                <input type="submit" value="Submit" />
+            </fieldset>
+        </form>
+    </body>
+</html>
