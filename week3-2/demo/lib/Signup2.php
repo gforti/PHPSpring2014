c6261a37f368371bde5da08689335a925828ab43
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-04-23 20:37:14 -0400

    demo week 3 stuff

--- week3-2/demo/lib/Signup2.php
+++ week3-2/demo/lib/Signup2.php
@@ -14,5 +14,65 @@
 class Signup2 {
     //put your code here
     
-    public $email;
+    private $email;
+    private $username;
+    private $password;
+    private $confirmPassword;
+    
+
+        public function __construct() {
+        $this->setEmail(filter_input(INPUT_POST, 'email'));
+        $this->username = filter_input(INPUT_POST, 'username');
+        $this->password = filter_input(INPUT_POST, 'password');
+        $this->confirmPassword = filter_input(INPUT_POST, 'confirmPassword');        
+    }
+    
+    
+    
+    /**
+    * A method to get email
+    *
+    * @return String
+    */ 
+    public function getEmail() {
+        return $this->email;
+    }
+
+    /**
+    * A method to set email
+    *
+    * @param string $email
+    * 
+    * @return void
+    */ 
+    private function setEmail($email) {
+        $this->email = $email;
+    }
+
+
+    
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getConfirmPassword() {
+        return $this->confirmPassword;
+    }
+
+    private function setUsername($username) {
+        $this->username = $username;
+    }
+
+    private function setPassword($password) {
+        $this->password = $password;
+    }
+
+    private function setConfirmPassword($confirmPassword) {
+        $this->confirmPassword = $confirmPassword;
+    }
+    
 }
