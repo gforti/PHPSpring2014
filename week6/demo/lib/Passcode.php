595c4b2acd339ca4541bce94a38abe796e459bb3
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-05-07 18:34:21 -0400

    week6

---
+++ week6/demo/lib/Passcode.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Passcode
+ *
+ * @author GFORTI
+ */
+class Passcode {
+    //put your code here
+    
+    private $passcode;
+    
+    function __construct() {
+        
+    }
+
+    public function getPasscode() {
+        return $this->passcode;
+    }
+
+    public function setPasscode($passcode) {
+        $this->passcode = $passcode;
+    }
+
+
+    
+}
