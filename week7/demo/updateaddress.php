5342837c14cac060977464639269ebbf11f18a7e
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-05-14 18:19:13 -0400

    week7

---
+++ week7/demo/updateaddress.php
@@ -0,0 +1,29 @@
+<?php include 'dependency.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+        Util::confirmAccess();
+      
+        
+         $address = new AddressBook();
+         
+         //$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+         $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+         
+         $addressResult = $address->read($id);
+          print_r($addressResult);
+          
+          
+          echo $addressResult['address'];
+          
+        
+        ?>
+    </body>
+</html>
