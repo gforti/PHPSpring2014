a8a7a9b33c48797a969b73015dc484db8c962e35
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-05-21 19:25:09 -0400

    week8

--- week8/demo/forumQuestion.php
+++ week8/demo/forumQuestion.php
@@ -11,36 +11,69 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        
+        /*
                 
-        //PHP Syntax is different than other languages, but resembles C++ the most.  Please answer all of the questions with code examples.
+        PHP Syntax is different than other languages, but resembles C++ the most.  Please answer all of the questions with code examples.
 
-       // 1.  How do you create a variable. Set the variable to a string with the word "hello".
-       
-        
-       // 2.  What is the syntax to call a public method/function from a Class.
-        
-       // 3.  What is the syntax to call a private method/function from inside the Class
-       
-       // 4.  What is the syntax to call a public static method/function
-       
-       // 5.  How do I access an associative array vs a index array
-        
-       // 6.  How can I access the global post variable.  
-       
-       // 7.  How do I create a Class.
-       
-       // 8.  How do I extend a Class.
-       
-       // 9.  How do I output a message.
-       
-       // 10. How do I outpu a message with PHP variables
-       
-       // 11. How do I call a PHP built-in function.
+        1. How do you create a variable. Set the variable to a string with the word "hello".
+        $myvar = "hello";
 
-        
-        
-        
-        
+        2. What is the syntax to call a public method/function from a Class.
+        $classname = new Class();
+        $classname->method();
+
+        3. What is the syntax to call a private method/function from inside the Class
+
+        $this->method();
+
+        4. What is the syntax to call a public static method/function
+
+        Class::method();
+
+        5. How do I access an associative array vs. an index array
+
+        $arr = array("key" => "value");
+
+        $arr[0] = "value";
+
+        array["key"]
+        array[3]
+
+        6. How can I access the global post variable.  
+
+        filter_input(INPUT_POST, "key");
+
+        $_POST["key"]
+
+        7. How do I create a Class.
+
+        class Hello {}
+
+        8. How do I extend a Class.
+
+        class World extends Hello {}
+
+        9. How do I output a message.
+
+        echo "message";
+
+        10.How do I output a message with PHP variables
+
+        $myvar = "hello";
+
+        echo $myvar, 'world';
+
+        echo $myvar.'world';
+
+        echo "$myvar world";
+
+        11.How do I call a PHP built-in function.
+
+
+        sha1("test");
+
+        */
         
         
         ?>
