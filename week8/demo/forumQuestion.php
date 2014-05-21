dfdc0d5321d4cbc72bc8f174837d6660f89f34e3
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-05-21 19:05:56 -0400

    week8

---
+++ week8/demo/forumQuestion.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+                
+        //PHP Syntax is different than other languages, but resembles C++ the most.  Please answer all of the questions with code examples.
+
+       // 1.  How do you create a variable. Set the variable to a string with the word "hello".
+       
+        
+       // 2.  What is the syntax to call a public method/function from a Class.
+        
+       // 3.  What is the syntax to call a private method/function from inside the Class
+       
+       // 4.  What is the syntax to call a public static method/function
+       
+       // 5.  How do I access an associative array vs a index array
+        
+       // 6.  How can I access the global post variable.  
+       
+       // 7.  How do I create a Class.
+       
+       // 8.  How do I extend a Class.
+       
+       // 9.  How do I output a message.
+       
+       // 10. How do I outpu a message with PHP variables
+       
+       // 11. How do I call a PHP built-in function.
+
+        
+        
+        
+        
+        
+        
+        ?>
+    </body>
+</html>
